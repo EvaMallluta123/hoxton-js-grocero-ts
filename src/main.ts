2923abea7825e90d18da8c9578e3134d4bc1e128
Eva Malluta <eva.malluta14@gmail.com>
2022-06-28 21:39:21 +0200

    change

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,48 @@
 import './style.css'
-
-/*
-
-This is how an item object should look like
-
-{
-  id: 1, // <- the item id matches the icon name in the assets/icons folder
-  name: "beetroot",
-  price: 0.35 // <- You can come up with your own prices
+type State= {item:item[]}
+type item={
+    id: number;
+    name: string;
+    price: number;
+    incart: number;
+    img: string;
 }
-
-*/
-// - A user can view a selection of items in the store
-// - From the store, a user can add an item to their cart
-// - From the cart, a user can view and adjust the number of items in their cart
-// - If an item's quantity equals zero it is removed from the cart
-// - A user can view the current total in their cart
-
-
-// - Create a state object
-// - Create action functions that update state
-// - Create render functions  that read from state and update the visuals
-
-
-let state= {
-  count:0,
+let state:State= {
   item:[
   {id:1,
     name:"beetroot", 
     price:0.35,
-    incart: 2,
+    incart: 9,
     img: "./icons/001-beetroot.svg"
     },
   {id:2,
     name:"carrot", 
     price:0.35,
-    incart: 2,
+    incart: 5,
     img:"./icons/002-carrot.svg"
   },
   {id:3,
     name:"apple", 
     price:0.35,
-    incart: 2,
+    incart: 76,
     img:"./icons/003-apple.svg"
   },
   {id:4,
     name:"apricot", 
     price:0.35,
-    incart: 2,
+    incart: 100,
     img:"./icons/004-apricot.svg"
   },
   {id:5,
     name:"avocado", 
     price:0.35,
-    incart: 2,
+    incart: 22,
     img:"./icons/005-avocado.svg"
   },
   {id:6,
     name:"bananas", 
     price:0.35,
-    incart: 2,
+    incart: 1,
     img:"./icons/006-bananas.svg"
   },
   {id:7,
@@ -71,24 +54,27 @@ let state= {
   {id:8,
     name:"berry", 
     price:0.35,
-    incart: 2,
+    incart: 9,
     img:"./icons/008-berry.svg"
   },
   {id:9,
     name:"blueberry", 
     price:0.35,
-    incart: 2,
+    incart: 0,
     img:"./icons/009-blueberry.svg"
   },
   {id:10,
     name:"eggplant ", 
     price:0.35,
-    incart: 2,
+    incart: 7,
     img:"./icons/010-eggplant.svg"
   }
 ]
 }
-
+function addimageStoreItrem(item:any){
+  let id=String(item.id).padStart(3,"0")
+  return `./icons/${id}-${item.name}.svg`
+}
 function renderStoreItem(){
 let ulEl = document.querySelector(".item-list.store--item-list")
 let itemLiEl= document.createElement("li")
@@ -97,9 +83,12 @@ itemUlDiv.className="store--item-icon"
 for(let item of state.item){
 let itemUlImg= document.createElement("img")
 let itemUlButton= document.createElement("button")
-  itemUlImg.src=item.img
+  itemUlImg.src=addimageStoreItrem(item)
 itemUlImg.alt=item.name
 itemUlButton.textContent="Add to cart"
+itemUlButton.addEventListener('click', function () {
+  incrise(item)
+})
 itemUlDiv.append(itemUlImg,itemUlButton)
 
 }
@@ -108,7 +97,14 @@ itemLiEl.append(itemUlDiv)
 ulEl.append(itemLiEl)
 
 }
+//get the item that are in thje cart
+function getItemcart(){
+  return state.item.filter(item=> item.incart>0)
+}
+let card= getItemcart()
 function renderCartItem(){
+  // document.body.textContent=``
+  for( let item of card){
 let ulEl = document.querySelector(".item-list.cart--item-list")
 
 let itemLiEl= document.createElement("li")
@@ -117,32 +113,36 @@ itemImg.src="cart--item-icon"
 itemImg.className=""
 itemImg.alt=""
 let itemp= document.createElement("p")
-itemp.textContent="beetroot"
+itemp.textContent=item.name
 let itemButton= document.createElement("button")
 itemButton.className="remove-btn"
 itemButton.textContent="-"
-itemButton.addEventListener('click', function () {
-  // update state
-  state.count--
-  // rerender
-  render()
+itemButton.addEventListener('click', function (item) {
+  discrise(item)
 })
 let itemSpan= document.createElement("span")
 itemSpan.className="quantity-text"
-itemSpan.textContent=state.count
+itemSpan.textContent=String(item.incart)
 let itemButton1= document.createElement("button")
 itemButton1.className="add-btn"
 itemButton1.textContent="+"
-itemButton1.addEventListener('click', function () {
-    // update state
-    state.count++
-    // rerender
-    render()
+itemButton1.addEventListener('click', function (item) {
+  incrise(item)
   })
 itemLiEl.append(itemImg, itemp, itemButton, itemSpan, itemButton1)
 
 ulEl.append(itemLiEl)
+  // }
 
+}
+}
+function incrise(item){
+  item.incard++
+  render()
+}
+function discrise(item){
+  item.incard--
+  render()
 }
 function render(){
   // document.body.textContent=""
